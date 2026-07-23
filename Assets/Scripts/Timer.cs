7b67047e45ff919037c3e05c3265bd30d2ffa173
Yayul225 <loayzamedina@gmail.com>
2026-07-23 13:49:33 -0400

    Cambio en script timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,4 +1,4 @@
-﻿/*using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -62,4 +62,4 @@ public class Timer : MonoBehaviour
         uiFill.fillAmount = 0;
         Debug.Log("Finalizado");
     }
-}*/
+}
