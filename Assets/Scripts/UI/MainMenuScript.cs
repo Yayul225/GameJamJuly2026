a4727f20ba786d18be2d48e87b05bf3760e58284
Yayul225 <loayzamedina@gmail.com>
2026-07-23 22:16:35 -0400

    Revert "PickUp Funcional y prueba de tiles"
    This reverts commit c634eaa141eb67bf0c172daf43e632139e3d58b5.

--- Assets/Scripts/UI/MainMenuScript.cs
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -5,7 +5,7 @@ public class MainMenuScript : MonoBehaviour
 {
     public void Play()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Exit()
